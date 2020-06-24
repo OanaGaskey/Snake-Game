2c74b50ad00259b142c7efd2d5446611a6fdefc9
Oana <oana.gaskey@gmail.com>
2020-06-24 17:32:30 -0400

    implemented controls for the second snake

--- src/controller.cpp
+++ src/controller.cpp
@@ -9,7 +9,7 @@ void Controller::ChangeDirection(Snake &snake, Snake::Direction input,
   return;
 }
 
-void Controller::HandleInput(bool &running, Snake &snake) const {
+void Controller::HandleInput(bool &running, Snake &snake1, Snake &snake2) const {
   SDL_Event e;
   while (SDL_PollEvent(&e)) {
     if (e.type == SDL_QUIT) {
@@ -17,22 +17,42 @@ void Controller::HandleInput(bool &running, Snake &snake) const {
     } else if (e.type == SDL_KEYDOWN) {
       switch (e.key.keysym.sym) {
         case SDLK_UP:
-          ChangeDirection(snake, Snake::Direction::kUp,
+          ChangeDirection(snake1, Snake::Direction::kUp,
                           Snake::Direction::kDown);
           break;
 
         case SDLK_DOWN:
-          ChangeDirection(snake, Snake::Direction::kDown,
+          ChangeDirection(snake1, Snake::Direction::kDown,
                           Snake::Direction::kUp);
           break;
 
         case SDLK_LEFT:
-          ChangeDirection(snake, Snake::Direction::kLeft,
+          ChangeDirection(snake1, Snake::Direction::kLeft,
                           Snake::Direction::kRight);
           break;
 
         case SDLK_RIGHT:
-          ChangeDirection(snake, Snake::Direction::kRight,
+          ChangeDirection(snake1, Snake::Direction::kRight,
+                          Snake::Direction::kLeft);
+          break;
+          
+        case SDLK_w:
+          ChangeDirection(snake2, Snake::Direction::kUp,
+                          Snake::Direction::kDown);
+          break;
+          
+          case SDLK_s:
+          ChangeDirection(snake2, Snake::Direction::kDown,
+                          Snake::Direction::kUp);
+          break;
+
+        case SDLK_a:
+          ChangeDirection(snake2, Snake::Direction::kLeft,
+                          Snake::Direction::kRight);
+          break;
+
+        case SDLK_d:
+          ChangeDirection(snake2, Snake::Direction::kRight,
                           Snake::Direction::kLeft);
           break;
       }
